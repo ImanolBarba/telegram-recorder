41632e6cfe0262ae1d247faf1b033553ed54fb88
Imanol-Mikel Barba Sabariego <imanol@imanolbarba.net>
2022-06-18 22:11:38 +0100

    Added LRU

---
+++ lru.hpp
@@ -0,0 +1,81 @@
+//
+// Copyright (c) 2022, Imanol-Mikel Barba Sabariego
+// All rights reserved.
+// 
+// Distributed under BSD 3-Clause License. See LICENSE.
+
+#ifndef LRU_HPP
+#define LRU_HPP
+
+#include <map>
+#include <vector>
+
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#include <spdlog/spdlog.h>
+
+template<class K, class V>
+class LRU {
+  public:
+    LRU(unsigned int size) : size(size) {};
+    V* get(K key);
+    unsigned int numItems();
+    void put(K key, V value);
+    void evict(K key);
+
+  private:
+    std::map<K, V> data;
+    std::vector<K> itemList;
+    unsigned int size;
+};
+
+template<class K, class V>
+V* LRU<K, V>::get(K key) {
+  if(this->data.find(key) == this->data.end()) {
+    return NULL;
+  }
+  return &this->data[key];
+}
+
+template<class K, class V>
+unsigned int LRU<K, V>::numItems() {
+  return this->itemList.size();
+}
+
+template<class K, class V>
+void LRU<K, V>::put(K key, V value) {
+  if(this->data.find(key) != this->data.end()) {
+    for(unsigned int i = 0; i < this->numItems(); i++) {
+      if(this->itemList[i] == key) {
+        this->itemList.erase(this->itemList.begin() + i);
+        this->itemList.insert(this->itemList.begin(), key);
+        this->data[key] = value;
+        return;
+      }
+    }
+    SPDLOG_ERROR("Inconsistency between cache map and list");
+  } else {
+    this->itemList.insert(this->itemList.begin(), key);
+    this->data[key] = value;
+    for(unsigned int i = this->numItems(); i > this->size; i--) {
+      this->data.erase(this->itemList[i-1]);
+      this->itemList.pop_back();
+    }
+  }
+}
+
+template<class K, class V>
+void LRU<K, V>::evict(K key) {
+  if(this->data.find(key) != this->data.end()) {
+    for(unsigned int i = 0; i < this->itemList.size(); ++i) {
+      if(this->itemList[i] == key) {
+        this->itemList.erase(this->itemList.begin() + i);
+        this->data.erase(key);
+        return;
+      }
+    }
+  } else {
+    SPDLOG_WARN("Asked to evict key {}, which is not cached", key);
+  }
+}
+
+#endif
--- telegram_recorder.hpp
+++ telegram_recorder.hpp
@@ -34,43 +34,43 @@ class TelegramRecorder {
     void start();
     void stop();
 
- private:
- void runRecorder();
-  bool loadConfig();
-  void restart();
-  void sendQuery(
-    td_api::object_ptr<td_api::Function> func,
-    std::function<void(TDAPIObjectPtr)> handler
-  );
-  void processResponse(td::ClientManager::Response response);
-  void processUpdate(TDAPIObjectPtr update);
-  auto createAuthQueryHandler();
-  void onAuthStateUpdate();
-  void checkAuthError(TDAPIObjectPtr object);
-  void enqueueMessageToRead(std::shared_ptr<td_api::message>& message);
-  void enqueueMessageToWrite(std::shared_ptr<td_api::message>& message);
-  void runMessageReader();
-  void markMessageAsRead(td_api::int53 chatID, td_api::int53 messageID);
-  bool writeMessageToDB(std::shared_ptr<td_api::message>& message);
-  void runDBWriter();
-  bool initDB();
+  private:
+    void runRecorder();
+    bool loadConfig();
+    void restart();
+    void sendQuery(
+      td_api::object_ptr<td_api::Function> func,
+      std::function<void(TDAPIObjectPtr)> handler
+    );
+    void processResponse(td::ClientManager::Response response);
+    void processUpdate(TDAPIObjectPtr update);
+    auto createAuthQueryHandler();
+    void onAuthStateUpdate();
+    void checkAuthError(TDAPIObjectPtr object);
+    void enqueueMessageToRead(std::shared_ptr<td_api::message>& message);
+    void enqueueMessageToWrite(std::shared_ptr<td_api::message>& message);
+    void runMessageReader();
+    void markMessageAsRead(td_api::int53 chatID, td_api::int53 messageID);
+    bool writeMessageToDB(std::shared_ptr<td_api::message>& message);
+    void runDBWriter();
+    bool initDB();
 
-  std::unique_ptr<td::ClientManager> clientManager;
-  std::int32_t clientID{0};
-  td_api::object_ptr<td_api::AuthorizationState> authState;
-  bool authorized{false};
-  bool needRestart{false};
-  std::uint64_t currentQueryID{0};
-  std::uint64_t authQueryID{0};
-  std::map<std::uint64_t, std::function<void(TDAPIObjectPtr)>> handlers;
-  std::atomic<bool> exitFlag{false};
-  std::map<td_api::int53, std::vector<std::shared_ptr<td_api::message>>> toReadMessageQueue;
-  std::map<td_api::int53, std::vector<std::shared_ptr<td_api::message>>> toWriteMessageQueue;
-  std::mutex toReadQueueMutex;
-  std::mutex toWriteQueueMutex;
-  std::condition_variable messagesAvailableToWrite;
-  ConfigParams config;
-  sqlite3 *db;
+    std::unique_ptr<td::ClientManager> clientManager;
+    std::int32_t clientID{0};
+    td_api::object_ptr<td_api::AuthorizationState> authState;
+    bool authorized{false};
+    bool needRestart{false};
+    std::uint64_t currentQueryID{0};
+    std::uint64_t authQueryID{0};
+    std::map<std::uint64_t, std::function<void(TDAPIObjectPtr)>> handlers;
+    std::atomic<bool> exitFlag{false};
+    std::map<td_api::int53, std::vector<std::shared_ptr<td_api::message>>> toReadMessageQueue;
+    std::map<td_api::int53, std::vector<std::shared_ptr<td_api::message>>> toWriteMessageQueue;
+    std::mutex toReadQueueMutex;
+    std::mutex toWriteQueueMutex;
+    std::condition_variable messagesAvailableToWrite;
+    ConfigParams config;
+    sqlite3 *db;
 };
 
 #endif
